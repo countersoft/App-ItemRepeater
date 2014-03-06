6e422fad9d605ad8ad2981f68ebe44b44c74e064
gurindergulati <gurinder.gulati@countersoft.com>
2014-03-06 17:32:22 +0000

    initial setup

---
+++ ItemRepeater.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Countersoft.Foundation.Commons.Extensions;
+using Countersoft.Gemini.Commons;
+using Countersoft.Gemini.Commons.Dto;
+using Countersoft.Gemini.Commons.Entity;
+using Countersoft.Gemini.Commons.Meta;
+using Countersoft.Gemini.Commons.System;
+using Countersoft.Gemini.Contracts.Business;
+using Countersoft.Gemini.Infrastructure.Managers;
+using Countersoft.Gemini.Infrastructure.TimerJobs;
+using Countersoft.Gemini.Extensibility.Apps;
+
+namespace ItemRepeater
+{
+    [AppType(AppTypeEnum.Timer),
+    AppGuid("CEF9D547-5106-49D6-8B08-F753AE1E23BF"),
+    AppName("Item Repeater"),
+    AppDescription("Repeats items at specified intervals")]
+    public class RepeatEngine : TimerJob
+    {
+        public override bool Run(IssueManager issueManager)
+        {
+/*#if DEBUG
+            Debugger.Launch();
+#endif*/
+            LogDebugMessage("Repeat Processing");
+
+            try
+            {
+                List<IssueDto> issues = issueManager.GetRepeated();
+
+                if (issues == null || issues.Count == 0) return true;
+
+                List<IssueLinkType> linkTypes = issueManager.GeminiContext.Meta.LinkTypeGet();
+
+                IssueLinkType repeatedLinkType = linkTypes.Find(t => string.Compare(t.Label, "Repeated", true) == 0);
+
+                if (repeatedLinkType == null && linkTypes.Count > 0) repeatedLinkType = linkTypes[0];
+
+                foreach (IssueDto issue in issues)
+                {
+                    RepeatParser repeat = new RepeatParser(issue.Repeated);
+
+                    repeat.CurrentDateTime = DateTime.UtcNow.ToLocal(issueManager.UserContext.User.TimeZone);
+                                        
+                    IssueDto lastRepeated = issueManager.GetLastCreatedIssueForOriginator(IssueOriginatorType.Repeat, issue.Id.ToString());
+
+                    DateTime lastRepeatedDate = issue.Created;
+
+                    if (lastRepeated != null && lastRepeated.Entity.IsExisting) lastRepeatedDate = lastRepeated.Created;
+
+                    if (repeat.MaximumRepeats > 0)
+                    {
+                        List<IssueDto> repeatedIssues = issueManager.GetItemsForOriginator(IssueOriginatorType.Repeat, issue.Id.ToString());
+
+                        if (repeatedIssues != null && repeatedIssues.Count >= repeat.MaximumRepeats) continue;
+                    }
+
+                    if (repeat.NeedsToRepeat(lastRepeatedDate))
+                    {
+                        var customFields = issue.CustomFields;
+                        
+                        issue.Attachments = new List<IssueAttachmentDto>();
+
+                        issue.Entity.Repeated = string.Empty;
+
+                        issue.Entity.OriginatorData = issue.Entity.Id.ToString();
+
+                        issue.Entity.OriginatorType = IssueOriginatorType.Repeat;
+
+                        issue.Entity.ParentIssueId = null;
+
+                        issue.Entity.StatusId = 0;
+
+                        issue.Entity.ResolutionId = 0;
+
+                        if (issue.Entity.StartDate.HasValue && issue.Entity.DueDate.HasValue
+                            && issue.Entity.StartDate != new DateTime() && issue.Entity.DueDate != new DateTime())
+                        {
+                            TimeSpan tsDates = issue.Entity.DueDate.Value - issue.Entity.StartDate.Value;
+
+                            issue.Entity.DueDate = DateTime.Today.AddDays(tsDates.TotalDays);
+
+                            issue.Entity.StartDate = DateTime.Today;
+                        }
+                        else
+                        {
+                            issue.Entity.StartDate = null;
+
+                            issue.Entity.DueDate = null;
+                        }
+
+                        int issueId = issue.Id;
+
+                        IssueDto repeated = issueManager.Create(issue.Entity);
+
+                        if (customFields != null && customFields.Count > 0)
+                        {
+                            var customFieldManager = new CustomFieldManager(issueManager);
+
+                            foreach (var field in customFields)
+                            {
+                                try
+                                {
+                                    field.Entity.Id = 0;
+
+                                    field.Entity.IssueId = repeated.Entity.Id;
+
+                                    field.Entity.ProjectId = repeated.Entity.ProjectId;
+
+                                    customFieldManager.Update(new CustomFieldData(field.Entity));
+                                }
+                                catch (Exception ex)
+                                {
+                                    LogException(ex);
+                                }
+                            }
+                        }
+
+                        if (repeatedLinkType != null)
+                        {
+                            issueManager.IssueLinkCreate(repeated.Entity.Id, issueId, repeatedLinkType.Id);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogException(ex);
+            }
+
+            return true;
+        }
+
+        public override void Shutdown()
+        {
+            
+        }
+
+        public override TimerJobSchedule GetInterval(IGlobalConfigurationWidgetStore dataStore)
+        {
+            var data = dataStore.Get<TimerJobSchedule>(AppGuid);
+
+            if (data == null || data.Value == null || (data.Value.Cron.IsEmpty() && data.Value.IntervalInHours.GetValueOrDefault() == 0 && data.Value.IntervalInMinutes.GetValueOrDefault() == 0))
+            {
+                return new TimerJobSchedule(60);
+            }
+
+            return data.Value;
+        }
+
+        public override void SetInterval(IGlobalConfigurationWidgetStore dataStore, TimerJobSchedule schedule)
+        {
+            dataStore.Save<TimerJobSchedule>(AppGuid, schedule);   
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ItemRepeater")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ItemRepeater")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("28b5f8e7-b69e-4c9c-859b-af6b18c6d237")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("6.1.0.4081")]
+[assembly: AssemblyVersion("6.1.0.4081")]
+[assembly: AssemblyFileVersion("6.1.0.4081")]
